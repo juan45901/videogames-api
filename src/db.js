0d90bacc8e6dffdbecc4555ad7afa91ba035f981
juan45901 <jgcompraslogistica@gmail.com>
2022-10-25 17:15:53 -0500

    frist

--- src/db.js
+++ src/db.js
@@ -30,7 +30,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/adgdzhpl`,
 
         {
           logging: false,
